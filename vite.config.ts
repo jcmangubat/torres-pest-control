cec7b6339a58c7de10f4f7592ceb89843bd2c7ae
James Mangubat <jc.mangubat@hotmail.com>
2025-07-18 20:03:47 +0800

    GH-Pages Updates

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/torres-pest-control/",
   server: {
     host: "::",
     port: 8081,
