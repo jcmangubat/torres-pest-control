85884ca7a7e2bed7eb0a2771da7afe7f2165d958
James Mangubat <jc.mangubat@hotmail.com>
2025-07-18 23:57:07 +0800

    Deploy fix

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/torres-pest-control">
+        <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
